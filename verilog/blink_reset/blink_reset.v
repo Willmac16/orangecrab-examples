e9cbfb35b88e923a9de765da4e0e18ca906f8c4b
Will MacCormack <will.maccormack@fireflyspace.com>
2025-08-10 11:53:18 -0500

    Gitignores & press to led; hold to reset

--- verilog/blink_reset/blink_reset.v
+++ verilog/blink_reset/blink_reset.v
@@ -27,15 +27,20 @@ module top (
     // Output inverted values of counter onto LEDs
     assign rgb_led0_r = ~counter[24];
     assign rgb_led0_g = ~counter[25];
-    assign rgb_led0_b = 1;
+    assign rgb_led0_b = usr_btn;
 
-    // Reset logic on button press.
-    // this will enter the bootloader
-    reg reset_sr = 1'b1;
+    // Reset logic on button hold.
+    // Create a press and hold reset counter
+    reg [25:0] rst_cnt = 0;
+
+    // Every button press increment register by 1
     always @(posedge clk48) begin
-        reset_sr <= {usr_btn};
+        if (~usr_btn) begin
+          rst_cnt <= rst_cnt + 1;
+        end else begin
+          rst_cnt <= 0;
+        end
     end
-    assign rst_n = reset_sr;
 
-
+    assign rst_n = ~rst_cnt[25];
 endmodule
